85a8b5191160a6926b53cff1dbceaa859e8fffc5
Arielle <arieloring@gmail.com>
2019-08-03 00:08:58 +0000

    Add active record validation for all input options

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, :address, :description, presence: true, length: { minimum: 3 }
+  validates :name, :address, length: { maximum: 140 }
+  validates :description, length: { maximum: 2000 }
 end
