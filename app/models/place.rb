bd42f83e31124a5d5a90c7efb7df5604edc05d2d
Arielle <arieloring@gmail.com>
2019-08-07 01:35:49 +0000

    Add Comments model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
-  after_validation :geocode 
+  after_validation :geocode
+  
   validates :name, :address, :description, presence: true, length: { minimum: 3 }
   validates :name, :address, length: { maximum: 140 }
   validates :description, length: { maximum: 2000 }
