a3c9fe234c827d1fcd8bef3f0ecd7fa69bc2c23f
Arielle <arieloring@gmail.com>
2019-08-12 04:37:52 +0000

    add photo functionality

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :photo
 
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, :address, :description, presence: true, length: { minimum: 3 }
   validates :name, :address, length: { maximum: 140 }
   validates :description, length: { maximum: 2000 }
